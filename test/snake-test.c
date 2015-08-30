fc4b96cb2722521f7e4970808333643fa3b18a0c
Kenan SUN <kenansun0@gmail.com>
2015-08-30 14:11:58 +0800

    [2015/08/30] Added unit test

---
+++ test/snake-test.c
@@ -0,0 +1,33 @@
+#include "snake.h"
+#include "stdio.h"
+
+void printNode(int x, int y) {
+  printf("Node(x, y) = (%d, %d)\n", x, y);
+}
+int main(int argc, char *argv[]) {
+  int x, y;
+  int i;
+  Snake *s = SnakeInit(10, 10);
+  for (i = 0 ; i < 10; ++i) {
+	  x = 13 - i;
+	  y = 10;
+	  printf("(%d, %d) in snake: %d\n", x, y, InSnake(x, y));
+  }
+  ForeachSnakeNode(printNode);
+  printf("Moved...\n");
+  SnakeMove();
+  ForeachSnakeNode(printNode);
+  printf("Turned DOWN and Moved...\n");
+  SnakeTurn(DOWN);
+  SnakeMove();
+  ForeachSnakeNode(printNode);
+  printf("Grown and Moved...\n");
+  SnakeGrow();
+  SnakeMove();
+  ForeachSnakeNode(printNode);
+  return 0;
+}
+
+
+
+
